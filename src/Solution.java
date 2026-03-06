e86f8a656b681d110fa34ed4602f03dc9274697d
Gowtham Krishna P K <gowthamkrishnapk05@gmail.com>
2026-03-06 10:03:24 +0530

    updated

--- src/Solution.java
+++ src/Solution.java
@@ -1,20 +1,23 @@
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        LinkedHashMap<String,Integer> wordCount = new LinkedHashMap<>();
-
-        while(sc.hasNext()){
-            String word = sc.next();
-            wordCount.put(word, wordCount.getOrDefault(word,0)+1);
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        String[] words = text.split("\\s+");
+        Map<String, Integer> frequencyMap = new HashMap<>();
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                frequencyMap.put(word, frequencyMap.getOrDefault(word, 0) + 1);
+            }
         }
-
-        for(Map.Entry<String,Integer> entry : wordCount.entrySet()){
-            System.out.println(entry.getKey()+": "+entry.getValue());
+        String[] resultParts = new String[frequencyMap.size()];
+        int i = 0;
+        for (Map.Entry<String, Integer> entry : frequencyMap.entrySet()) {
+            resultParts[i++] = entry.getKey() + ": " + entry.getValue();
         }
+        System.out.println(String.join(" ", resultParts));
     }
 }
