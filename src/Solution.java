2d55c1bf4263a583211d4f9ba675c4401e297ac3
Gowtham Krishna P K <gowthamkrishnapk05@gmail.com>
2026-03-06 09:51:25 +0530

    updated

--- src/Solution.java
+++ src/Solution.java
@@ -6,21 +6,15 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // LinkedHashMap preserves the order in which keys are first inserted
-        LinkedHashMap<String, Integer> wordCount = new LinkedHashMap<>();
+        LinkedHashMap<String,Integer> wordCount = new LinkedHashMap<>();
 
-        // Read all words from the input
-        while (sc.hasNext()) {
+        while(sc.hasNext()){
             String word = sc.next();
-            // Increment the count or set to 1 if it's new
-            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+            wordCount.put(word, wordCount.getOrDefault(word,0)+1);
         }
 
-        // Print the results (will now follow the input order)
-        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
+        for(Map.Entry<String,Integer> entry : wordCount.entrySet()){
+            System.out.println(entry.getKey()+": "+entry.getValue());
         }
-
-        sc.close();
     }
 }
